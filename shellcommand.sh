9d9d24dda0cb683f6a246ac1387e1f9cea54819d
wangyingtaodeepin <wangyingtao@linuxdeepin.com>
2016-05-11 16:26:19 +0800

    Add wifi command

--- shellcommand.sh
+++ shellcommand.sh
@@ -2,5 +2,7 @@
 
 dmidecode | grep -A 8 "System Information"
 lspci
+nmcli dev wifi
+echo "------------------------------------------------------------"
 deepin-feedback-cli --dump basic
 deepin-feedback-cli --dump device
