40fad10f3128dbe334bc4003f54309b0df4776fe
wangyingtaodeepin <wangyingtao@linuxdeepin.com>
2016-06-08 15:16:14 +0800

    Add sleep time for adjust serial

--- shellcommand.sh
+++ shellcommand.sh
@@ -3,6 +3,7 @@
 dmidecode | grep -A 8 "System Information"
 lspci
 nmcli dev wifi
+sleep 7200
 echo "------------------------------------------------------------"
 deepin-feedback-cli --dump basic
 deepin-feedback-cli --dump device
