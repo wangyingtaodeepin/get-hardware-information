7aaaefacf6ac81d8dd030563a0bae7b539050298
wangyingtaodeepin <wangyingtao@linuxdeepin.com>
2016-06-15 11:31:11 +0800

    delete sleep time

--- shellcommand.sh
+++ shellcommand.sh
@@ -3,7 +3,6 @@
 dmidecode | grep -A 8 "System Information"
 lspci
 nmcli dev wifi
-sleep 7200
 echo "------------------------------------------------------------"
 deepin-feedback-cli --dump basic
 deepin-feedback-cli --dump device
